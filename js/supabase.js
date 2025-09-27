72b3d6783a7d7ba006dff4f958bfc878fd24ef6c
Connor O'Brien <connorbobrien@gmail.com>
2025-09-27 20:12:00 -0300

    Update Supabase CDN

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 const supabaseUrl = "";
 const supabaseKey = "";
